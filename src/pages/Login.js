29cf3720f6a20dd5118fb7e1a3cb4edccb15cd25
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-08-01 17:16:49 -0400

    Requisito 1 done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,100 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import emailAction from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+      disable: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => { this.isDisabledButton(); });
+  };
+
+  isDisabledButton = () => {
+    const { email, password } = this.state;
+    const emailValided = /\S+@\S+\.\S+/;
+    const minCharacters = 6;
+
+    if (password.length >= minCharacters && emailValided.test(email)) {
+      this.setState({
+        disable: false,
+      }, () => {});
+    } else {
+      this.setState({
+        disable: true,
+      });
+    }
+  }
+
+  clickButton = (e) => {
+    const { history, addUserDispatch } = this.props;
+    const { email } = this.state;
+    addUserDispatch(email);
+    e.preventDefault();
+    history.push('/carteira');
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disable } = this.state;
+
+    return (
+      <div>
+        <form>
+          <label htmlFor="email-input">
+            <input
+              data-testid="email-input"
+              type="email"
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label
+            htmlFor="password"
+          >
+            <input
+              type="password"
+              data-testid="password-input"
+              name="password"
+              onChange={ this.handleChange }
+              value={ password }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ disable }
+            onClick={ this.clickButton }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  addUserDispatch: (payload) => dispatch(emailAction(payload)),
+});
+
+Login.propTypes = {
+  addUserDispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,9 @@
 // Coloque aqui suas actions
+export const ADD_EMAIL = 'ADD_EMAIL';
+
+const emailAction = (email) => ({
+  type: ADD_EMAIL,
+  payload: email,
+});
+
+export default emailAction;
