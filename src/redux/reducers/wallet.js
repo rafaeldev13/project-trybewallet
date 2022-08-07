88fb3fd746f1b25fac720303d04fcf67e4d23e6f
Rafael-Alencar <rafael.13.alencar13@gmail.com>
2022-08-07 18:10:54 -0400

    Erro no requisito 4, finalizando o Requisito 6

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -3,6 +3,7 @@ import { GET_COIN } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
+  expenses: [],
 };
 
 const wallet = (state = INITIAL_STATE, action) => {
@@ -11,6 +12,11 @@ const wallet = (state = INITIAL_STATE, action) => {
     return {
       ...state,
       currencies: action.payload,
+    };
+  case 'GET_EXPENSES':
+    return {
+      ...state,
+      expenses: [...state.expenses, { ...action.expenses }],
     };
   default:
     return state;
